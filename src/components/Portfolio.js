c402bfab84ae9988a85020cf4d5c50a976ee73b1
Kota Ueshima <ueshima@usc.edu>
2022-10-28 13:43:46 -0700

    added link

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -10,31 +10,37 @@ function Portfolio() {
       id: 1,
       src: globify,
       codeLink: "https://github.com/KotaUeshima/Capstone-Project-Frontend",
+      demoLink: "https://stately-bombolone-0812fa.netlify.app/",
     },
     {
       id: 2,
       src: college_marketplace,
-      codeLink: "https://github.com/KotaUeshima/College_marketplace_frontend",
+      codeLink: "https://github.com/KotaUeshima/college_marketplace_frontend",
+      demoLink: "https://clever-kulfi-41745d.netlify.app/",
     },
     {
       id: 3,
       src: traveler,
       codeLink: "https://github.com/KotaUeshima/traveler-guide",
+      demoLink: "https://clever-kulfi-41745d.netlify.app/",
     },
     {
       id: 4,
       src: coming_soon,
       codeLink: "https://github.com/KotaUeshima/College_marketplace_frontend",
+      demoLink: "https://clever-kulfi-41745d.netlify.app/",
     },
     {
       id: 5,
       src: coming_soon,
       codeLink: "https://github.com/KotaUeshima/College_marketplace_frontend",
+      demoLink: "https://clever-kulfi-41745d.netlify.app/",
     },
     {
       id: 6,
       src: coming_soon,
       codeLink: "https://github.com/KotaUeshima/College_marketplace_frontend",
+      demoLink: "https://clever-kulfi-41745d.netlify.app/",
     },
   ];
   return (
@@ -53,14 +59,14 @@ function Portfolio() {
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src, codeLink }) => (
+          {portfolios.map(({ id, src, codeLink, demoLink }) => (
             <div
               key={id}
               className="shadow-md shadow-gray-600 rounded-md hover:scale-105 duration-200"
             >
               <img src={src} alt="" />
               <div className="flex items-center justify-center">
-                <a href="https://github.com/KotaUeshima/College_marketplace_frontend">
+                <a href={demoLink} target="_blank" rel="noreferrer">
                   <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
                     Demo
                   </button>
