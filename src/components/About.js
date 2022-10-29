514e5cf92e629aeab4a61be391bcc1a9b918fde6
Kota Ueshima <ueshima@usc.edu>
2022-10-28 18:51:19 -0700

    added projects

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from "react";
 function About() {
   return (
     <div name="about" className="w-full h-screen bg-white text-black">
-      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+      <div className="max-w-screen-md p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8 text-center">
           <p className="text-4xl font-extrabold tracking-widest inline border-b-4 border-pink-800">
             ABOUT ME
@@ -11,26 +11,24 @@ function About() {
         </div>
 
         <p className="text-lg text-center text-gray-500 mt-16">
-          I'm a senior at the Unviersity of Southern California, who has
-          recently transitioned into{" "}
-          <span className="font-bold text-black">
-            Full-Stack Web Development.
-          </span>{" "}
-          I have a minor in Computer Programming, and just completed my time at{" "}
+          Hey everyone! My name is Kota Ueshima and I'm a senior at the
+          Unviersity of Southern California, with a passion for learning new
+          technologies and solving modern day problems. I have a minor in{" "}
+          <span className="font-bold text-black">Computer Programming</span>,
+          and have just completed my time at{" "}
           <span className="font-bold text-black">
             Flatiron's Software Engineering Bootcamp
           </span>
-          . I am excited to start my journey into tech and want to be at a
-          constant state of learning.
+          . I am excited to start my journey into tech and am open to
+          opportunities for experience.
         </p>
         <br />
         <p className="text-lg text-center  text-gray-500">
-          I have changed career paths{" "}
-          <span className="font-bold text-pink-800">3</span> times, lived in{" "}
-          <span className="font-bold text-pink-800">3</span> different
-          continents, and I pride myself on adaptability and perseverance. I am
-          open to job opportunities and want to be at a community where I can
-          grow, and make an impact.
+          As a busines administration major, I have strong teamworking and
+          communication skills, which are vital in the software field. I have
+          lived in <span className="font-bold text-pink-800">3</span> different
+          countries, Japan, England, and the US, and I pride myself on
+          adaptability and perseverance. Thanks so much, and enjoy my portfolio!
         </p>
       </div>
     </div>
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,45 +2,37 @@ import React from "react";
 import globify from "../assets/portfolio/globify.png";
 import college_marketplace from "../assets/portfolio/college_marketplace.png";
 import traveler from "../assets/portfolio/traveler.png";
-import coming_soon from "../assets/portfolio/coming_soon.png";
+import { FaGithub } from "react-icons/fa";
 
 function Portfolio() {
   const portfolios = [
     {
       id: 1,
       src: globify,
-      codeLink: "https://github.com/KotaUeshima/Capstone-Project-Frontend",
+      title: "Globify",
+      technologies: "React.js | Ruby on Rails | Bootstrap",
+      frontendLink: "https://github.com/KotaUeshima/globify-frontend",
+      backendLink: "https://github.com/KotaUeshima/globify-backend",
       demoLink: "https://stately-bombolone-0812fa.netlify.app/",
     },
     {
       id: 2,
       src: college_marketplace,
-      codeLink: "https://github.com/KotaUeshima/college_marketplace_frontend",
+      title: "College Marketplace",
+      technologies: "React.js | Ruby | MUI",
+      frontendLink:
+        "https://github.com/KotaUeshima/college_marketplace_frontend",
+      backendLink: "https://github.com/KotaUeshima/college_marketplace_backend",
       demoLink: "https://clever-kulfi-41745d.netlify.app/",
     },
     {
       id: 3,
       src: traveler,
-      codeLink: "https://github.com/KotaUeshima/traveler-guide",
-      demoLink: "https://clever-kulfi-41745d.netlify.app/",
-    },
-    {
-      id: 4,
-      src: coming_soon,
-      codeLink: "https://github.com/KotaUeshima/College_marketplace_frontend",
-      demoLink: "https://clever-kulfi-41745d.netlify.app/",
-    },
-    {
-      id: 5,
-      src: coming_soon,
-      codeLink: "https://github.com/KotaUeshima/College_marketplace_frontend",
-      demoLink: "https://clever-kulfi-41745d.netlify.app/",
-    },
-    {
-      id: 6,
-      src: coming_soon,
-      codeLink: "https://github.com/KotaUeshima/College_marketplace_frontend",
-      demoLink: "https://clever-kulfi-41745d.netlify.app/",
+      title: "Traveler's Guide",
+      technologies: "React.js | MUI",
+      frontendLink: "https://github.com/KotaUeshima/traveler-guide",
+      backendLink: "https://github.com/KotaUeshima/travelers-guide-backend",
+      demoLink: "https://fluffy-vacherin-e1c347.netlify.app",
     },
   ];
   return (
@@ -59,26 +51,47 @@ function Portfolio() {
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src, codeLink, demoLink }) => (
-            <div
-              key={id}
-              className="shadow-md shadow-gray-600 rounded-md hover:scale-105 duration-200"
-            >
-              <img src={src} alt="" />
-              <div className="flex items-center justify-center">
-                <a href={demoLink} target="_blank" rel="noreferrer">
-                  <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                    Demo
-                  </button>
-                </a>
-                <a href={codeLink} target="_blank" rel="noreferrer">
-                  <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                    Code
-                  </button>
-                </a>
+          {portfolios.map(
+            ({
+              id,
+              src,
+              title,
+              technologies,
+              frontendLink,
+              backendLink,
+              demoLink,
+            }) => (
+              <div
+                key={id}
+                className="shadow-md shadow-gray-600 rounded-md hover:scale-105 duration-200"
+              >
+                <img className="border-b-8 border-pink-800" src={src} alt="" />
+                <p className="text-center pt-2 text-xl font-bold">{title}</p>
+                <p className="text-center text-gray-500 text-sm">
+                  {technologies}
+                </p>
+                <div className="flex items-center justify-center">
+                  <a href={demoLink} target="_blank" rel="noreferrer">
+                    <button className="px-6 pt-3 m-2 text-pink-800 duration-200 hover:scale-105">
+                      Demo
+                    </button>
+                  </a>
+                </div>
+                <div className="flex items-center justify-center">
+                  <a href={frontendLink} target="_blank" rel="noreferrer">
+                    <button className="w-1/2 px-6 pb-3 m-4 duration-200 hover:scale-105">
+                      Frontend <FaGithub />
+                    </button>
+                  </a>
+                  <a href={backendLink} target="_blank" rel="noreferrer">
+                    <button className="w-1/2 px-6 pb-3 m-4 duration-200 hover:scale-105">
+                      Backend <FaGithub />
+                    </button>
+                  </a>
+                </div>
               </div>
-            </div>
-          ))}
+            )
+          )}
         </div>
       </div>
     </div>
